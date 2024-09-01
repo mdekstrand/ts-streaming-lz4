d03b4b89d9686691a84bba4ceffad56916e8d04f
Michael Ekstrand <md@ekstrandom.net>
2024-09-01 13:19:26 -0400

    stream

--- src/stream.rs
+++ src/stream.rs
@@ -16,6 +16,9 @@ extern "C" {
 }
 
 /// Stream-format LZ4 encoder.
+///
+/// A stream encoder implements Deno's `WriterSync` writes to an underlying
+/// `WriterSync`.
 #[wasm_bindgen]
 pub struct LZ4EncoderStream {
     encoder: FrameEncoder<JSWriter>,
