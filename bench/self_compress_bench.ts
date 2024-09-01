0dd12b0d5cd799f4afce5eccf61709e6c3706845
Michael Ekstrand <md@ekstrandom.net>
2024-09-01 15:01:56 -0400

    benchmark multiple compressed file sizes

--- bench/self_compress_bench.ts
+++ bench/self_compress_bench.ts
@@ -15,14 +15,17 @@ const files = {
     "pkg/streaming_lz4_bg.wasm",
   ),
   cli: await Deno.readFile("target/debug/streaming-lz4"),
+  deno: await Deno.readFile(Deno.execPath()),
 };
 
 for (const [name, data] of Object.entries(files)) {
   console.info("source %s: %d bytes", name, data.length);
 
-  Deno.bench("compress_block", { group: name, baseline: true }, () => {
-    compress_block(data);
-  });
+  if (name != "deno") {
+    Deno.bench("compress_block", { group: name, baseline: true }, () => {
+      compress_block(data);
+    });
+  }
 
   Deno.bench("compress_framed", { group: name }, () => {
     compress_framed(data);
